0584707c8bc25b5f475f1f1fbd76f9741004c283
Oran Dan <oran-dan@hotmail.com>
2023-11-26 14:29:07 +0100

    added all obv3 examples as json_files for testing, finally i did not pursue testing the builders. This will be discussed in the next meeting

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,14 +1,1 @@
-mod achievement_credential;
-
-use serde::de::DeserializeOwned;
-use std::fs::File;
-use std::path::Path;
-
-pub fn json_example<T>(path: &str) -> T
-where
-    T: DeserializeOwned,
-{
-    let file_path = Path::new(path);
-    let file = File::open(file_path).expect("file does not exist");
-    serde_json::from_reader::<_, T>(file).expect("could not parse json")
-}
+mod obv3_examples_tests;
