623344b30935978e13e12fbf5289ec2dc56aedb3
joris <joris.willems@impierce.com>
2024-04-24 16:58:14 +0200

    rename dir

--- crates/validator/src/validator.rs
+++ crates/validator/src/validator.rs
@@ -22,8 +22,8 @@ impl ValidateRequest {
 
 fn validate_shacl(json_file: &PathBuf) -> io::Result<bool> {
     let manifest_dir = manifest_dir();
-    let cwd = manifest_dir.join("src/py-shacl-validator/");
-    let python_bin = manifest_dir.join("src/py-shacl-validator/venv/bin/python");
+    let cwd = manifest_dir.join("src/shacl-validator/");
+    let python_bin = manifest_dir.join("src/shacl-validator/venv/bin/python");
 
     let out = Command::new(python_bin)
         .current_dir(cwd)
