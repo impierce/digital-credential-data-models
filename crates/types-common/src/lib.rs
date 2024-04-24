cba6be9aaa7f921424cb544e4a539304b4584ced
joris <joris.willems@impierce.com>
2024-04-24 11:10:29 +0200

    update elm types

--- crates/types-common/src/lib.rs
+++ crates/types-common/src/lib.rs
@@ -7,46 +7,10 @@ pub enum ObjectOrVector<T> {
     Vector(Vec<T>),
 }
 
-impl<T: Clone> From<&ObjectOrVector<T>> for ObjectOrVector<T> {
-    fn from(value: &ObjectOrVector<T>) -> Self {
-        value.clone()
-    }
-}
-
-impl<T> From<T> for ObjectOrVector<T> {
-    fn from(value: T) -> Self {
-        Self::Object(value)
-    }
-}
-
-impl<T> From<Vec<T>> for ObjectOrVector<T> {
-    fn from(value: Vec<T>) -> Self {
-        Self::Vector(value)
-    }
-}
-
 /// Very large structs use box construct to prevent too much memory from being copied.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(untagged)]
 pub enum BoxObjectOrVector<T> {
     Object(Box<T>),
     Vector(Vec<T>),
-}
-
-impl<T: Clone> From<&BoxObjectOrVector<T>> for BoxObjectOrVector<T> {
-    fn from(value: &BoxObjectOrVector<T>) -> Self {
-        value.clone()
-    }
-}
-
-impl<T> From<T> for BoxObjectOrVector<T> {
-    fn from(value: T) -> Self {
-        Self::Object(Box::new(value))
-    }
-}
-
-impl<T> From<Vec<T>> for BoxObjectOrVector<T> {
-    fn from(value: Vec<T>) -> Self {
-        Self::Vector(value)
-    }
 }
