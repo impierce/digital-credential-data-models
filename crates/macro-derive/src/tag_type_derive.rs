537f636a24a7be6386406dda601b427e472ae51e
joris <joris.willems@impierce.com>
2024-05-07 12:11:57 +0700

    Add tag function on struct
    now you can do
    
    Self::tag() to retrieve the correct type

--- crates/macro-derive/src/tag_type_derive.rs
+++ crates/macro-derive/src/tag_type_derive.rs
@@ -4,15 +4,21 @@ use syn::{parse_macro_input, DeriveInput};
 pub fn impl_tag_type(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let tag_value = &input.ident;
-    let tag_struct = format_ident!("{}Tag", input.ident);
+    let tag_source = &input.ident;
+    let tag_target = format_ident!("{}Tag", input.ident);
 
     let expand = quote! {
+        impl #tag_source {
+            pub fn tag() -> #tag_target {
+                #tag_target::default()
+            }
+        }
+
         /// Tag matching the struct name
         #[derive(Clone, Debug, serde::Serialize)]
-        pub struct #tag_struct(String);
+        pub struct #tag_target(String);
 
-        impl ::std::ops::Deref for #tag_struct {
+        impl ::std::ops::Deref for #tag_target {
             type Target = String;
 
             fn deref(&self) -> &Self::Target {
@@ -20,13 +26,13 @@ pub fn impl_tag_type(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
             }
         }
 
-        impl Default for #tag_struct {
+        impl Default for #tag_target {
             fn default() -> Self {
-                Self(stringify!(#tag_value).to_string())
+                Self(stringify!(#tag_source).to_string())
             }
         }
 
-        impl<'de> Deserialize<'de> for #tag_struct {
+        impl<'de> Deserialize<'de> for #tag_target {
             fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
             where
                 D: serde::Deserializer<'de>,
@@ -34,13 +40,13 @@ pub fn impl_tag_type(input: proc_macro::TokenStream) -> proc_macro::TokenStream 
                 let val = String::deserialize(deserializer)?;
 
                 // if &val != "Person"
-                if &val != stringify!(#tag_value) {
+                if &val != stringify!(#tag_source) {
                     Err(serde::de::Error::invalid_value(
                         serde::de::Unexpected::Str(&val),
-                        &stringify!(#tag_value),
+                        &stringify!(#tag_source),
                     ))
                 } else {
-                    Ok(#tag_struct(val))
+                    Ok(#tag_target(val))
                 }
             }
         }
