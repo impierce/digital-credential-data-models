a88d4bd9023f61458f901c266dc60c45bf2a6960
joris <joris.willems@impierce.com>
2024-05-17 17:15:34 +0700

    improve schema stuff

--- crates/types-common/src/traits.rs
+++ crates/types-common/src/traits.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{collections::HashMap, fmt, path::PathBuf};
 
 use chrono::{DateTime, TimeZone, Utc};
 use macro_derive::GenPaths;
@@ -8,11 +8,33 @@ pub enum Multiplicity {
     Many,
 }
 
+impl fmt::Display for Multiplicity {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::One => f.write_str("1"),
+            Self::Many => f.write_str("*"),
+        }
+    }
+}
+
 pub struct SchemaData {
     pub src_schema: String,
     pub json_path: String,
     pub tgt_schema: String,
     pub multiplicity: Multiplicity,
+    // TODO add optionality var
+}
+
+impl fmt::Display for SchemaData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!(
+            "{}, {}, {}, {}",
+            self.src_schema,
+            self.json_path,
+            self.tgt_schema,
+            self.multiplicity,
+        ))
+    }
 }
 
 pub trait AddSchemaTypes {
@@ -34,63 +56,63 @@ pub struct Person {
 }
 
 //impl AddSchemaTypes for Option<bool> {
-    //fn add_schema_types(map: &mut HashMap<String, Vec<SchemaData>>) {
+//fn add_schema_types(map: &mut HashMap<String, Vec<SchemaData>>) {
 
-        //if map.contains_key("Options") {
-            //let data = map.get_mut("Options").unwrap();
+//if map.contains_key("Options") {
+//let data = map.get_mut("Options").unwrap();
 
-            //if data.iter().all(|d| &d.tgt_schema != "bool") {
-                //data.push(SchemaData {
-                    //..
-                //})
-                    
-            //}  
-        //}
-    //}
+//if data.iter().all(|d| &d.tgt_schema != "bool") {
+//data.push(SchemaData {
+//..
+//})
+
+//}
+//}
+//}
 //}
 
 //impl AddSchemaTypes for Person {
-    //fn add_schema_types(map: &mut HashMap<String, Vec<SchemaData>>) {
-        //// Type has already been added
-        //if map.contains_key("Person") {
-            //return;
-        //}
+//fn add_schema_types(map: &mut HashMap<String, Vec<SchemaData>>) {
+//// Type has already been added
+//if map.contains_key("Person") {
+//return;
+//}
 
-        //let mut data = vec![];
+//let mut data = vec![];
 
-        //data.push(SchemaData {
-            //src_schema: "Person".to_string(),
-            //json_path: "$.name".to_string(),
-            //multiplicity: Multiplicity::One,
-            //tgt_schema: "String".to_string(),
-        //});
+//data.push(SchemaData {
+//src_schema: "Person".to_string(),
+//json_path: "$.name".to_string(),
+//multiplicity: Multiplicity::One,
+//tgt_schema: "String".to_string(),
+//});
 
-        //data.push(SchemaData {
-            //src_schema: "Person".to_string(),
-            //json_path: "$.sur_name".to_string(),
-            //multiplicity: Multiplicity::One,
-            //tgt_schema: "String".to_string(),
-        //});
+//data.push(SchemaData {
+//src_schema: "Person".to_string(),
+//json_path: "$.sur_name".to_string(),
+//multiplicity: Multiplicity::One,
+//tgt_schema: "String".to_string(),
+//});
 
-        //data.push(SchemaData {
-            //src_schema: "Person".to_string(),
-            //json_path: "$.age".to_string(),
-            //multiplicity: Multiplicity::One,
-            //tgt_schema: "usize".to_string(),
-        //});
+//data.push(SchemaData {
+//src_schema: "Person".to_string(),
+//json_path: "$.age".to_string(),
+//multiplicity: Multiplicity::One,
+//tgt_schema: "usize".to_string(),
+//});
 
-        //data.push(SchemaData {
-            //src_schema: "Person".to_string(),
-            //json_path: "$.address".to_string(),
-            //multiplicity: Multiplicity::One,
-            //tgt_schema: "Address".to_string(),
-        //});
+//data.push(SchemaData {
+//src_schema: "Person".to_string(),
+//json_path: "$.address".to_string(),
+//multiplicity: Multiplicity::One,
+//tgt_schema: "Address".to_string(),
+//});
 
-        //map.insert("Person".to_string(), data);
+//map.insert("Person".to_string(), data);
 
-        //// If generc
-        //String::add_schema_types(map);
-    //}
+//// If generc
+//String::add_schema_types(map);
+//}
 //}
 
 // Implement empty traits impl for all types
@@ -107,15 +129,13 @@ impl_T!(for isize, i8, i16, i32, i64, i128);
 impl_T!(for String, bool, PathBuf);
 
 //impl<T: TimeZone> AddSchemaTypes for DateTime<T> {
-    //fn add_schema_types(_map: &mut HashMap<String, Vec<SchemaData>>) {}
+//fn add_schema_types(_map: &mut HashMap<String, Vec<SchemaData>>) {}
 //}
 
-
 //impl AddSchemaTypes for String {
-    //fn add_schema_types(_map: &mut HashMap<String, Vec<SchemaData>>) {}
+//fn add_schema_types(_map: &mut HashMap<String, Vec<SchemaData>>) {}
 //}
 
 //impl AddSchemaTypes for usize {
-    //fn add_schema_types(_map: &mut HashMap<String, Vec<SchemaData>>) {}
+//fn add_schema_types(_map: &mut HashMap<String, Vec<SchemaData>>) {}
 //}
-
